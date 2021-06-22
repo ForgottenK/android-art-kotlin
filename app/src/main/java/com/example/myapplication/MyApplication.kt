fecf3343c196f809eaaf313ebe86b89fc41b3419
Wang RuiXiang <wrx340083063@gmail.com>
2021-06-22 20:06:11 +0800

    MyApplication add delay tasks and uses threadPool & countDownLatch to parallel execute them

--- app/src/main/java/com/example/myapplication/MyApplication.kt
+++ app/src/main/java/com/example/myapplication/MyApplication.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import com.example.myapplication.chapter_1.model.db.AppDatabase
 import com.example.myapplication.chapter_1.model.repository.PostRepository
 import com.example.myapplication.chapter_13.CrashHandler
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
 
 /**
  *    @author wangruixiang
@@ -22,6 +24,60 @@ class MyApplication : Application() {
         //在这里为应用设置异常处理程序，然后我们的程序才能捕获未处理的异常
         val crashHandler = CrashHandler.instance
         crashHandler.init(this)
+
+        initOtherModules()
+    }
+
+    private fun initOtherModules() {
+        val countDownLatch = CountDownLatch(3)
+        val newFixedThreadPool = Executors.newFixedThreadPool(4)
+        newFixedThreadPool.submit {
+            initModuleA()
+        }
+        newFixedThreadPool.submit {
+            initModuleD()
+            countDownLatch.countDown()
+        }
+        newFixedThreadPool.submit {
+            initModuleF()
+            countDownLatch.countDown()
+        }
+        newFixedThreadPool.submit {
+            initModuleG()
+            countDownLatch.countDown()
+        }
+        countDownLatch.await()
+        initModuleB()
+        initModuleC()
+        initModuleE()
+    }
+
+    private fun initModuleA() {
+        Thread.sleep(100)
+    }
+
+    private fun initModuleB() {
+        Thread.sleep(10)
+    }
+
+    private fun initModuleC() {
+        Thread.sleep(30)
+    }
+
+    private fun initModuleD() {
+        Thread.sleep(200)
+    }
+
+    private fun initModuleE() {
+        Thread.sleep(50)
+    }
+
+    private fun initModuleF() {
+        Thread.sleep(300)
+    }
+
+    private fun initModuleG() {
+        Thread.sleep(500)
     }
 
     companion object {
